46bcc77e8f6bc56273a31233419e32efa1a0b909
Tejaswiberad20 <tejaswiberad2004@gmail.com>
2024-04-28 10:19:50 +0530

    local storage add

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+let todoList = [];
 
-
-    const todoList = [];
+const listFromLocalStorage = localStorage.getItem("todoList");
+if(listFromLocalStorage)
+{
+    todoList = JSON.parse(listFromLocalStorage);
+    renderToDoList();
+}
 
 function add(){
     const inputElement = document.getElementById('input-todo');
     todoList.push(inputElement.value);
-    inputElement.value = ""; 
+    
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    inputElement.value = "";
 
     renderToDoList();
 }
@@ -13,6 +20,7 @@ function add(){
 function renderToDoList(){
     const listContainer = document.getElementById('list-container');
     listContainer.innerHTML = "";
+
     for(const todoItem of todoList)
     {
         listContainer.innerHTML += `<div class="todoItem">
